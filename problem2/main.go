28b358752c327ce64f1f56d9d5b450689549f450
1ki10 <rifqiokta10@gmail.com>
2024-05-22 10:31:16 +0700

    tugas2

---
+++ problem2/main.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	//input
+	var alas float64 = 20
+	var tinggi float64 = 25
+
+	//code disini
+
+	// Rumus luas segitiga: 1/2 * alas * tinggi
+	luas := 0.5 * alas * tinggi
+
+	fmt.Println(luas)
+}
