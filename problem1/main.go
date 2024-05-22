31d4598d7f35abce0a4bc25a6b9da5bc8013986e
1ki10 <rifqiokta10@gmail.com>
2024-05-22 10:46:37 +0700

    tugas4

---
+++ problem1/main.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func playWithAsterik(n int) {
+	for i := 1; i <= n; i++ {
+		for j := 1; j <= n-i; j++ {
+			fmt.Print(" ")
+		}
+		for j := 1; j <= i; j++ {
+			fmt.Print("* ")
+		}
+		fmt.Println()
+	}
+}
+
+func main() {
+	playWithAsterik(5)
+}
