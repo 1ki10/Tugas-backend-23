5e735b85d328f8c501e38fb8053e63e220a300a3
1ki10 <rifqiokta10@gmail.com>
2024-05-22 10:30:25 +0700

    tugas2

---
+++ problem1/main.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	// input
+	var nama string = "Kobar"
+
+	//code disini
+
+	output := "Hello " + nama + "! Saya Golang, bahasa yang sangat menyenangkan"
+
+	// Mencetak hasil output
+	fmt.Println(output)
+}
